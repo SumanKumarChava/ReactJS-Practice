e3bcd785dc83217a664780cc88109098ff40a7e6
Suman Chava <Suman.Chava@blueyonder.com>
2024-02-20 23:14:09 +0530

    Complicated example with react

--- App.js
+++ App.js
@@ -1,5 +1,26 @@
-const heading = React.createElement("h1",{id:"header", name:"suman"},"Hello from React JS");
-console.log(heading);
+/*
+<div id="parent">
+    <div id="section1">
+        <h1>this is h1</h1>
+        <h2>this is h2</h2>
+    </div>
+    <div id="section2">
+        <h1>this is h1</h1>
+        <h2>this is h2</h2>
+    </div>
+</div>    
+
+Creating the above structure using react js
+*/
+
+
+const parent = React.createElement("div",{id:"parent"},
+[
+    React.createElement("div",{id:"section1"}, [React.createElement("h1",{},"this is h1"),React.createElement("h2",{},"this is h2")]),
+    React.createElement("div",{id:"section2"}, [React.createElement("h1",{},"this is h1"),React.createElement("h2",{},"this is h2")])
+]);
+
+
 const root = ReactDOM.createRoot(document.getElementById("div2"));
 console.log(root);
-root.render(heading);
+root.render(parent);
